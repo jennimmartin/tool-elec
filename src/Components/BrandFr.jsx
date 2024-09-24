6139a19e263dc2f4de1f2ac9d94cef8cb62bba51
Jenni <jennimmartin@outlook.com>
2024-09-24 15:47:38 +0200

    Added working with section En & Fr

---
+++ src/Components/BrandFr.jsx
@@ -0,0 +1,17 @@
+import { FrBrand } from "../data";
+import SectionTitle from "./SectionTitle";
+import { TrustCard } from "./TrustCard";
+
+const Brand = () => {
+  return (
+    <section className="py-20 align-center">
+      <SectionTitle text="Collaborer avec les meilleurs mar " />
+      <div className="py-16 grid gap-8 md:grid-col-2 lg: grid-cols-3">
+        {FrBrand.map((brand) => {
+          return <TrustCard key={brand.id} {...brand} />;
+        })}
+      </div>
+    </section>
+  );
+};
+export default Brand;

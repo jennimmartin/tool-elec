cb0cc1de17b3100f6308d57e74490fe434322d27
Jenni <jennimmartin@outlook.com>
2024-09-25 10:31:16 +0200

    typo

--- src/Components/BrandFr.jsx
+++ src/Components/BrandFr.jsx
@@ -5,7 +5,7 @@ import { TrustCard } from "./TrustCard";
 const Brand = () => {
   return (
     <section className="py-20 align-center">
-      <SectionTitle text="Collaborer avec les meilleurs mar " />
+      <SectionTitle text="Collaborer avec les meilleurs marques " />
       <div className="py-16 grid gap-8 md:grid-col-2 lg: grid-cols-3">
         {FrBrand.map((brand) => {
           return <TrustCard key={brand.id} {...brand} />;

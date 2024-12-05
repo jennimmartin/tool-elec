b45a544186fd2f4c2cd227005da0682eb87e648f
Jenni <jennimmartin@outlook.com>
2024-12-05 16:25:06 +0100

    object cover added to trust card

--- src/Components/TrustCard.jsx
+++ src/Components/TrustCard.jsx
@@ -1,7 +1,7 @@
 export const TrustCard = ({ img, title }) => {
   return (
     <article>
-      <span className="align-element grid justify-center items-center ">
+      <span className="align-element grid justify-center object-cover items-center ">
         <img src={img} alt={title} className="h-20 w-20" />
       </span>
     </article>

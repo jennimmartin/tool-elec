bd2f105f7eb2d447bdd3813f6a70aca5bcc5e89a
Jenni <jennimmartin@outlook.com>
2025-09-24 23:28:05 +0100

    home btn code change on legal page

--- src/Pages/Legal.jsx
+++ src/Pages/Legal.jsx
@@ -6,8 +6,8 @@ import LegalNotice from "../Components/LegalNotice";
 const Legal = () => {
   return (
     <div className="text-zinc-200 grid text-center justify-center py-20">
-      <Link to="/" className="py-8">
-        <AiFillHome className="text-[#c6210b] place-self-center hover:text-zinc-500 duration-300 w-8 h-8" />
+      <Link to="/" className="py-8 flex justify-center">
+        <AiFillHome className="text-[#c6210b] hover:text-zinc-500 duration-300 w-8 h-8" />
       </Link>
       <LegalNotice />
     </div>

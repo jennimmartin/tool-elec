1c3dc8d1f2308bc43468f8a1a0f78c545673238c
Jenni <jennimmartin@outlook.com>
2025-09-24 23:40:55 +0100

    use effect added to legal page to avoid link issue

--- src/Pages/Legal.jsx
+++ src/Pages/Legal.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
-
+import { useEffect } from "react";
 import LegalNotice from "../Components/LegalNotice";
 
 const Legal = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="text-zinc-200 grid text-center justify-center py-20">
       <Link to="/" className="py-8 flex justify-center">
@@ -13,4 +17,5 @@ const Legal = () => {
     </div>
   );
 };
+
 export default Legal;
